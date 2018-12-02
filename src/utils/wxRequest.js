c9b16fd5e850d7a0cc9bfe9e0d9a946fd995d233
hpzmanong <2985420850@qq.com>
2018-12-02 15:59:48 +0800

    request修改

--- src/utils/wxRequest.js
+++ src/utils/wxRequest.js
@@ -3,22 +3,53 @@ import util from './util';
 import md5 from './md5';
 import tip from './tip'
 
-const API_SECRET_KEY = 'http://ckj.zm.zhi-meng.net/admin'
-const TIMESTAMP = util.getCurrentTime()
-const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase())
-
-const wxRequest = async(params = {}, url) => {
+const API_SECRET_KEY = 'http://ckj.zm.zhi-meng.net/admin';
+const TIMESTAMP = util.getCurrentTime();
+const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase());
+const wxRequest=(params = {}, url)=>{
   tip.loading();
   let data = params.query.data || {};
-  let res = await wepy.request({
-    url: url,
-    method: params.method || 'POST',
-    data: data,
-    header: {  'Content-Type': 'application/x-www-form-urlencoded' },
+  return new Promise((resolve, reject)=>{
+    wepy.request({
+      url: url,
+      method: params.method || 'POST',
+      data: data,
+      header: {  'Content-Type': 'application/x-www-form-urlencoded' },
+      success:(res)=>{
+        resolve(res)
+      },
+      fail(res){
+        reject(res)
+      }
+    });
   });
   tip.loaded();
-  return res;
 };
+
+// const wxRequest = async(params = {}, url) => {
+//   tip.loading();
+//   let data = params.query.data || {};
+//
+//   let res = await wepy.request({
+//     url: url,
+//     method: params.method || 'POST',
+//     data: data,
+//     header: {  'Content-Type': 'application/x-www-form-urlencoded' },
+//     success:(res)=>{
+//       return res
+//     },
+//     fail(res){
+//       return res
+//     },
+//     complete:(res)=>{
+//       return res;
+//       console.log('res',res)
+//     }
+//   });
+//   tip.loaded();
+//
+//   return res;
+// };
 
 
 module.exports = {
